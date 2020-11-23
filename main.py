5f51e88c0cb19e813c5e01e10f522c48011f375c
Victor Poutot <v.poutot@gmail.com>
2020-11-23 16:00:26 +0100

    Meilleur méthode pour trouver la description

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
+from fonctions import liste_url_livre
 
 
-url='http://books.toscrape.com/catalogue/libertarianism-for-beginners_982/index.html'
-reponse=requests.get(url)
-if reponse.ok :
-    soup = BeautifulSoup(reponse.text, 'html.parser')
-    tabSoup=soup.find("table", {"class": "table table-striped"})  #je récupère le tableau d'inforamtion du produit
-    list=tabSoup.findAll('td') #liste contenant les cellules du tableau
-    UPC=list[0].text
-    price_exc=list[2].text.replace('Â','')
-    price_inc=list[3].text.replace('Â','')
-    dispo=list[5].text
-    reviews=list[6].text
+url='https://books.toscrape.com/catalogue/category/books/young-adult_21/index.html'
 
-    menuSoup=soup.find('ul', {"class": 'breadcrumb'}) #je récupère le sommaire pour la catégorie et le titre
-    categ=menuSoup.findAll('a')[-1].text
-    titre=menuSoup.find('li', {"class": "active"}).text
+liste_lien_livre=liste_url_livre(url)
+with open('fichier.csv', 'w') as file:
+    file.write( 'title; product_page_url; universal_ product_code (upc); price_including_tax; price_excluding_tax; number_available; product_description; category; review_rating;image_url\n')
+    for lien in liste_lien_livre :
+        reponse=requests.get(lien)
+        if reponse.ok :
+            soup = BeautifulSoup(reponse.text, 'html.parser')
+            tabSoup=soup.find("table", {"class": "table table-striped"})  #je récupère le tableau d'inforamtion du produit
+            liste_tab=tabSoup.findAll('td') #liste contenant les cellules du tableau
+            UPC=liste_tab[0].text
+            price_exc=liste_tab[2].text.replace('Â','')
+            price_inc=liste_tab[3].text.replace('Â','')
+            dispo=liste_tab[5].text
+            reviews=liste_tab[6].text
 
-    Description=soup.findAll('p')[3].text.replace(';',',') #Ici la description ne se trouve dans aucune division interessante, pas encore trouvé de meilleurs moyen que de récuperer tous les paragraphes...
+            menuSoup=soup.find('ul', {"class": 'breadcrumb'}) #je récupère le sommaire pour la catégorie et le titre
+            categ=menuSoup.findAll('a')[-1].text
+            titre=menuSoup.find('li', {"class": "active"}).text
 
-    image=soup.find('div', {'class':'item active'}).find('img')['src']#Je récupère l'url de l'image (incomplet)
-    image=image.replace('../..', 'http://books.toscrape.com')# Et je le complète
+            Description = soup.find('meta', {'name':'description'})['content'].replace(';',',').strip() #On récupère la description sans les points virgules et les saut de ligne pour le csv
+
+            image=soup.find('div', {'class':'item active'}).find('img')['src']#Je récupère l'url de l'image (incomplet)
+            image=image.replace('../..', 'http://books.toscrape.com')# Et je le complète
 
 
-    with open('fichier.csv','w') as file :
-        file.write('title; product_page_url; universal_ product_code (upc);price_including_tax,price_excluding_tax;number_available;product_description;category;review_rating;image_url\n')
-        file.write(titre + ';' + url + ';' + UPC + ';' + price_inc + ';' + price_exc + ';' + dispo + ';' + Description + ';' + categ + ';' + reviews + ';' + image)
+            file.write(titre + ' ; ' + url + ' ; ' + UPC + ' ; ' + price_inc + ' ; ' + price_exc + ' ; ' + dispo + ' ; ' + Description + ' ; ' + categ + ' ; ' + reviews + ' ; ' + image + '\n')
 
-else:
-    print(reponse)
+        else:
+            print(reponse)

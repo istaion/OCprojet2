fb69b8e59df0089e2c974426bc96f2227d0ef963
Victor Poutot <v.poutot@gmail.com>
2020-11-24 18:25:00 +0100

    Ajout du telechargement des images (989s d'execution en tout...)

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-import requests
 from bs4 import BeautifulSoup
 from fonctions import *
+import requests
+import shutil
 import time
 
-t1=time.time()
-
-url='https://books.toscrape.com/catalogue/category/books/travel_2/index.html'
-
+t=time.time()
 dictionnaire_cat=dico_url_cat()
-print(time.time()-t1)
-for categ, valeur in dictionnaire_cat.items():
 
+for categ, valeur in dictionnaire_cat.items():
     liste_lien_livre=liste_url_livre(valeur)
     with open(categ+'.csv', 'w') as file:
         file.write( 'title; product_page_url; universal_ product_code (upc); price_including_tax; price_excluding_tax; number_available; product_description; category; review_rating;image_url\n')
@@ -33,13 +30,15 @@ for categ, valeur in dictionnaire_cat.items():
 
                 image=soup.find('div', {'class':'item active'}).find('img')['src']#Je récupère l'url de l'image (incomplet)
                 image=image.replace('../..', 'http://books.toscrape.com')# Et je le complète
-
-
                 file.write(titre + ' ; ' + lien + ' ; ' + UPC + ' ; ' + price_inc + ' ; ' + price_exc + ' ; ' + dispo + ' ; ' + Description + ' ; ' + categ + ' ; ' + reviews + ' ; ' + image + '\n')
 
+                r = requests.get(image, stream=True)
+                if r.ok :
+                    titre=titre.replace('/','')
+                    with open(titre+'.jpeg', 'wb') as f:
+                        shutil.copyfileobj(r.raw, f)
+                else :
+                    print(r)
             else:
                 print(reponse)
-
-t=time.time()-t1
-
-print(t)
+print(time.time()-t)
